7a811efd2014d2f065857f0610bf15d4c29a82a4
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2020-08-18 10:49:10 -0600

    Agregando analizador lexico
    Agregando analizador lexico

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,13 @@
         {
             this.btnCargarArchivo = new System.Windows.Forms.Button();
             this.lblError = new System.Windows.Forms.Label();
-            this.rtbErrores = new System.Windows.Forms.RichTextBox();
             this.txtRuta = new System.Windows.Forms.TextBox();
+            this.lvToken = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // btnCargarArchivo
@@ -49,17 +54,9 @@
             this.lblError.AutoSize = true;
             this.lblError.Location = new System.Drawing.Point(12, 58);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(59, 17);
+            this.lblError.Size = new System.Drawing.Size(60, 17);
             this.lblError.TabIndex = 1;
-            this.lblError.Text = "Errores:";
-            // 
-            // rtbErrores
-            // 
-            this.rtbErrores.Location = new System.Drawing.Point(12, 91);
-            this.rtbErrores.Name = "rtbErrores";
-            this.rtbErrores.Size = new System.Drawing.Size(333, 516);
-            this.rtbErrores.TabIndex = 2;
-            this.rtbErrores.Text = "";
+            this.lblError.Text = "TOKEN:";
             // 
             // txtRuta
             // 
@@ -68,13 +65,53 @@
             this.txtRuta.Size = new System.Drawing.Size(924, 22);
             this.txtRuta.TabIndex = 3;
             // 
+            // lvToken
+            // 
+            this.lvToken.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvToken.HideSelection = false;
+            this.lvToken.Location = new System.Drawing.Point(15, 94);
+            this.lvToken.Name = "lvToken";
+            this.lvToken.Size = new System.Drawing.Size(1043, 513);
+            this.lvToken.TabIndex = 4;
+            this.lvToken.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Token";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Lexema";
+            this.columnHeader2.Width = 160;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Linea";
+            this.columnHeader3.Width = 160;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Columna";
+            this.columnHeader4.Width = 160;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Index";
+            this.columnHeader5.Width = 160;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1070, 619);
+            this.Controls.Add(this.lvToken);
             this.Controls.Add(this.txtRuta);
-            this.Controls.Add(this.rtbErrores);
             this.Controls.Add(this.lblError);
             this.Controls.Add(this.btnCargarArchivo);
             this.Name = "Form1";
@@ -88,8 +125,13 @@
 
         private System.Windows.Forms.Button btnCargarArchivo;
         private System.Windows.Forms.Label lblError;
-        private System.Windows.Forms.RichTextBox rtbErrores;
         private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.ListView lvToken;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,9 +14,10 @@ namespace minij
 {
     public partial class Form1 : Form
     {
-
+        String texto;
         OpenFileDialog open;
-        Lectura archivoLectura;
+        AnalizadorLexico archivoLectura;
+        List<string> palabrasReservadas;
 
         public Form1()
         {
@@ -30,7 +32,54 @@ namespace minij
             if (open.ShowDialog() == DialogResult.OK)
             {
                 txtRuta.Text = open.FileName;
-                archivoLectura = new Lectura(open.FileName);
+                archivoLectura = new AnalizadorLexico();
+                texto = File.ReadAllText(open.FileName);
+                AnalizarCodigo(open.FileName);
+            }
+        }
+
+        private void AnalizarCodigo(string ruta)
+        {
+            lvToken.View = View.Details;
+            using (StreamReader sr = new StreamReader(ruta))
+            {
+                //tbxCode.Text = sr.ReadToEnd();
+
+                archivoLectura.AddTokenRule(@"\s+", "ESPACIO", true);
+                archivoLectura.AddTokenRule(@"\b[_a-zA-Z][\w]*\b", "IDENTIFICADOR");
+                archivoLectura.AddTokenRule("\".*?\"", "CADENA");
+                archivoLectura.AddTokenRule(@"'\\.'|'[^\\]'", "CARACTER");
+                archivoLectura.AddTokenRule("//[^\r\n]*", "COMENTARIO1");
+                archivoLectura.AddTokenRule("/[*].*?[*]/", "COMENTARIO2");
+                archivoLectura.AddTokenRule(@"\d*\.?\d+", "NUMERO");
+                archivoLectura.AddTokenRule(@"[\(\)\{\}\[\];,]", "DELIMITADOR");
+                archivoLectura.AddTokenRule(@"[\.=\+\-/*%]", "OPERADOR");
+                archivoLectura.AddTokenRule(@">|<|==|>=|<=|!", "COMPARADOR");
+
+                palabrasReservadas = new List<string>() { "void","int","double","boolean","string",
+                "class","const","interface","null","null","this","extends","implements","for","while",
+                "if","else","return","New","System","out","println"};
+
+                archivoLectura.Compile(RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.ExplicitCapture);
+            }
+
+            lvToken.Items.Clear();
+
+            int n = 0, e = 0;
+
+            foreach (var tk in archivoLectura.GetTokens(texto))
+            {
+                if (tk.Name == "ERROR") e++;
+
+                if (tk.Name == "IDENTIFICADOR")
+                    if (palabrasReservadas.Contains(tk.Lexema))
+                        tk.Name = "RESERVADO";
+
+                ListViewItem lvi = new ListViewItem();
+                string[] row = { tk.Name, tk.Lexema, tk.Linea.ToString(), tk.Columna.ToString(), tk.Index.ToString()};
+                var listViewItem = new ListViewItem(row);
+                lvToken.Items.Add(listViewItem);
+                n++;
             }
         }
     }
